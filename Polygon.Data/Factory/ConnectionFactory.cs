50f93ef6531e76dd59edb6d77a7e00a6e257d631
gustavoosantoos <gustavoosantoos@outlook.com>
2018-06-23 12:39:10 -0300

    base connection string oracle

--- Polygon.Data/Factory/ConnectionFactory.cs
+++ Polygon.Data/Factory/ConnectionFactory.cs
@@ -12,6 +12,10 @@ namespace Polygon.Data.Factory
         private const string ConnectionString =
             "Server=localhost;Port=3306;Database=polygonpontoonline;UID=root;Pwd=!root;SslMode=none";
 
+        private const string OracleConnectionString =
+            "Data Source=(DESCRIPTION =(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST = 000.00.0.00)(PORT = 0000)))(CONNECT_DATA =(SERVICE_NAME = database)));User ID=User/Schema;Password=password;Unicode=True";
+
+
         public static IDbConnection GetConnection()
         {
             return new OracleConnection(ConnectionString);
